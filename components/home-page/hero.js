788c76fa020b0ea4462132c0bf56b9239500f2e6
Michael Nguyen <n93michael@gmail.com>
2022-09-10 19:34:40 -0400

    HTML structure done for hero component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
 function Hero() {
-  return <div>hero</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/michael.jpeg"
+          alt="An image showing Michael"
+          width={450}
+          height={600}
+        />
+      </div>
+      <h1>Hi! I'm Michael</h1>
+      <p>I blog about web development :)</p>
+    </section>
+  );
 }
 
 export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { Fragment } from 'react';
 
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
 const HomePage = () => {
   return (
     <Fragment>
