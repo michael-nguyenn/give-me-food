9095f8b1bd9828d018d9249399922b4abad49cb2
Michael Nguyen <n93michael@gmail.com>
2022-09-11 15:32:58 -0400

    styling done for post content and header

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,11 +1,21 @@
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
 
+const DUMMY_DATA = {
+  slug: 'test-michael',
+  title: 'This is a Test',
+  image: 'next-js.webp',
+  date: '2022-09-11',
+  content: '# This is a first post ',
+};
+
 function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_DATA.slug}/${DUMMY_DATA.image}`;
+
   return (
-    <article>
-      <PostHeader />
-      CONTENT
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_DATA.title} image={imagePath} />
+      {DUMMY_DATA.content}
     </article>
   );
 }
--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 function PostHeader(props) {
   const { title, image } = props;
   return (
-    <header>
+    <header className={classes.header}>
       <h1>{title}</h1>
       <Image src={image} alt={title} width={200} height={150} />
     </header>
