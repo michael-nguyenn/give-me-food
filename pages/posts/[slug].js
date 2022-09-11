dddb21bc4e1fd3c7dd02bffe03b65388605569d9
Michael Nguyen <n93michael@gmail.com>
2022-09-11 14:12:30 -0400

    added post header and content components

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,12 @@
+import { Fragment } from 'react';
+import PostContent from '../../components/posts/post-detail/post-content';
+
 const PostDetailPage = () => {
-  return <div>Welcome to Home!</div>;
+  return (
+    <Fragment>
+      <PostContent />
+    </Fragment>
+  );
 };
 
 export default PostDetailPage;
