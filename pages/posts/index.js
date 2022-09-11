b75fa1f2b920d781960ed13126170842b7a5680d
Michael Nguyen <n93michael@gmail.com>
2022-09-11 12:57:07 -0400

    created all posts component

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,36 @@
+import { Fragment } from 'react';
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_DATA = [
+  {
+    slug: 'test-michael',
+    title: 'This is a Test',
+    image: 'next-js.webp',
+    excerpt: 'This is a test data to make sure I did things properly',
+    date: '2022-09-11',
+  },
+  {
+    slug: 'test-michael-2',
+    title: 'This is a Test',
+    image: 'next-js.webp',
+    excerpt: 'This is a test data to make sure I did things properly',
+    date: '2022-09-11',
+  },
+  {
+    slug: 'test-michael-3',
+    title: 'This is a Test',
+    image: 'next-js.webp',
+    excerpt: 'This is a test data to make sure I did things properly',
+    date: '2022-09-11',
+  },
+];
+
 const AllPostsPage = () => {
-  return <div>Welcome to Home!</div>;
+  return (
+    <Fragment>
+      <AllPosts posts={DUMMY_DATA} />
+    </Fragment>
+  );
 };
 
 export default AllPostsPage;
