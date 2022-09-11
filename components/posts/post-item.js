0ece9a8b559799564a71b0bcd2c90003f81d7aae
Michael Nguyen <n93michael@gmail.com>
2022-09-11 11:48:12 -0400

    set up how post item data will be received via props. created date formating

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -2,17 +2,25 @@ import classes from './post-item.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-function PostItem() {
+function PostItem(props) {
+  const { title, excerpt, image, date, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long', // 1 ==> 'January'
+    year: 'numeric',
+  });
+
   return (
     <li className={classes.post}>
       <Link>
         <a>
           <div className={classes.image}>
-            <Image />
+            <Image src={image} alt="{title}" width={300} height={200} />
             <div className={classes.content}>
-              <h3>TITLE</h3>
-              <time>Sept 11th 2022</time>
-              <p>The content</p>
+              <h3>{title}</h3>
+              <time>{formattedDate}</time>
+              <p>{excerpt}</p>
             </div>
           </div>
         </a>
