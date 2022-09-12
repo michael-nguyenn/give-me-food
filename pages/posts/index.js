eee0ddbc77b0aa7db80911b19e671298921be084
Michael Nguyen <n93michael@gmail.com>
2022-09-12 09:20:29 -0400

    replaced dummy data from all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,36 +1,25 @@
 import { Fragment } from 'react';
 import AllPosts from '../../components/posts/all-posts';
 
-const DUMMY_DATA = [
-  {
-    slug: 'test-michael',
-    title: 'This is a Test',
-    image: 'next-js.webp',
-    excerpt: 'This is a test data to make sure I did things properly',
-    date: '2022-09-11',
-  },
-  {
-    slug: 'test-michael-2',
-    title: 'This is a Test',
-    image: 'next-js.webp',
-    excerpt: 'This is a test data to make sure I did things properly',
-    date: '2022-09-11',
-  },
-  {
-    slug: 'test-michael-3',
-    title: 'This is a Test',
-    image: 'next-js.webp',
-    excerpt: 'This is a test data to make sure I did things properly',
-    date: '2022-09-11',
-  },
-];
+import { getAllPosts } from '../../lib/posts-utils';
 
-const AllPostsPage = () => {
+const AllPostsPage = ({ posts }) => {
   return (
     <Fragment>
-      <AllPosts posts={DUMMY_DATA} />
+      <AllPosts posts={posts} />
     </Fragment>
   );
 };
+
+export function getStaticProps() {
+  const sortedPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: sortedPosts,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllPostsPage;
