4ab4e18ccca688e07c43079cb610de368a1e37e7
Michael Nguyen <n93michael@gmail.com>
2022-09-12 18:31:36 -0400

    added api end point for posting messages

---
+++ pages/api/contact.js
@@ -0,0 +1,33 @@
+function handler(req, res) {
+  if (req.method !== 'POST') return;
+
+  const { email, name, message } = req.body;
+
+  //validation
+  if (
+    !email ||
+    !email.includes('@') ||
+    !name ||
+    name.trim() === '' ||
+    !message ||
+    message.trim() === ''
+  ) {
+    res.status(422).json({ message: 'Invalid input.' });
+    return;
+  }
+
+  //store data in a db
+  const newMessage = {
+    email,
+    name,
+    message,
+  };
+
+  console.log(newMessage);
+
+  res
+    .status(201)
+    .json({ message: 'Successfully stored message!', message: newMessage });
+}
+
+export default handler;
